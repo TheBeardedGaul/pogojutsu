aac049a44d6d71cddcea0d37f08fc7d6a81c2d7f
TheBeardedGaul <florent.tetaud@gmail.com>
2020-01-19 23:42:13 +0100

    MVP: Update Fusion Cup data & Rose Cup data - Rose new meta - Fusion data - Rose data - missing pokemons added to pokedex for api back up - new entries for fast and charged moves

--- src/Pokemon/PokemonListComponent.tsx
+++ src/Pokemon/PokemonListComponent.tsx
@@ -4,10 +4,12 @@ import { Meta } from "../Data/SilphRoad/Meta/Meta";
 import { PokemonComponent } from "./PokemonComponent";
 
 export const PokemonListComponent: React.FC<any> = () => {
-    const { data, error } = useMetaRankedPokemon(Meta.Fusion);
+    // TODO hook for getting/setting the meta
+    const { data, error } = useMetaRankedPokemon(Meta.Rose);
     
     return (
         <>
+        {/* TODO component for meta switching */}
         {data.length > 0 && error === null && (
           <div className={"pokemonCardList"}>
           {data.map((element, index) => {
@@ -18,7 +20,7 @@ export const PokemonListComponent: React.FC<any> = () => {
 
         {error !== null && (
           <div className="error">
-            <strong>Error loading Pokemons from Meta {Meta.Fusion.toString()}.</strong>
+            <strong>Error loading Pokemons from Meta {Meta.Rose.toString()}.</strong>
             <br />
             {error}
           </div>
