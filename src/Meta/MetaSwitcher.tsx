161257d06140e386651a5411d9eed0ae7e1e4dd7
TheBeardedGaul <florent.tetaud@gmail.com>
2020-03-05 13:32:25 +0100

    MVP: style meta switcher as select

--- src/Meta/MetaSwitcher.tsx
+++ src/Meta/MetaSwitcher.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Meta } from "./Meta";
 import classNames from "classnames";
 import { Translate } from "../i18n";
+import { Select, MenuItem } from "@material-ui/core";
+import "./sass/MetaSwitcher.scss";
 
 export interface MetaSwitcherProps {
     meta: Meta;
@@ -19,12 +21,23 @@ export const MetaSwitcher: React.FC<MetaSwitcherProps> = ({meta, setMetaFct}) =>
     }
     return (
         <div className="languageButtonContainer">
-            <div className={getClassNames(meta === Meta.GoBattleLeague)}
+             <Select
+                className="MetaSelect"
+                labelId="selectMeta"
+                id="selectMeta"
+                value={meta}
+                onChange={(event) =>  setMetaFct((event.target.value as  Meta))}
+            >
+                <MenuItem className="MetaSelect" value={Meta.GoBattleLeague}>{<Translate id={`metas.${Meta.GoBattleLeague}`} />}</MenuItem>
+                <MenuItem className="MetaSelect" value={Meta.Rose}>{<Translate id={`metas.${Meta.Rose}`} />}</MenuItem>
+                <MenuItem className="MetaSelect" value={Meta.Toxic}>{<Translate id={`metas.${Meta.Toxic}`} />}</MenuItem>
+            </Select>
+            {/* <div className={getClassNames(meta === Meta.GoBattleLeague)}
                 onClick={() => setMetaFct(Meta.GoBattleLeague)}
             >
                 {<Translate id={`metas.${Meta.GoBattleLeague}`} />}
-            </div>
-            <div className={getClassNames(meta === Meta.Rose)}
+            </div> */}
+            {/* <div className={getClassNames(meta === Meta.Rose)}
                 onClick={() => setMetaFct(Meta.Rose)}
             >
                 {<Translate id={`metas.${Meta.Rose}`} />}
@@ -33,7 +46,7 @@ export const MetaSwitcher: React.FC<MetaSwitcherProps> = ({meta, setMetaFct}) =>
                 onClick={() => setMetaFct(Meta.Toxic)}
             >
                 {<Translate id={`metas.${Meta.Toxic}`} />}
-            </div>
+            </div> */}
         </div>
     );
 }
