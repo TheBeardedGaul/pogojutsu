d0098fd887697345b8e95e64c873234df9df2f8a
TheBeardedGaul <florent.tetaud@gmail.com>
2020-03-05 14:00:15 +0100

    MVP: league switcher as select and styling

--- src/Meta/MetaSwitcher.tsx
+++ src/Meta/MetaSwitcher.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Meta } from "./Meta";
-import classNames from "classnames";
 import { Translate } from "../i18n";
 import { Select, MenuItem } from "@material-ui/core";
 import "./sass/MetaSwitcher.scss";
@@ -11,16 +10,8 @@ export interface MetaSwitcherProps {
 }
 
 export const MetaSwitcher: React.FC<MetaSwitcherProps> = ({meta, setMetaFct}) => {
-    const getClassNames = (selected: boolean) => {
-        const componentClasses = classNames({
-            "languageButton": true,
-            "selectedButton": selected
-        });
-    
-        return componentClasses;
-    }
+
     return (
-        <div className="languageButtonContainer">
              <Select
                 className="MetaSelect"
                 labelId="selectMeta"
@@ -32,21 +23,5 @@ export const MetaSwitcher: React.FC<MetaSwitcherProps> = ({meta, setMetaFct}) =>
                 <MenuItem className="MetaSelect" value={Meta.Rose}>{<Translate id={`metas.${Meta.Rose}`} />}</MenuItem>
                 <MenuItem className="MetaSelect" value={Meta.Toxic}>{<Translate id={`metas.${Meta.Toxic}`} />}</MenuItem>
             </Select>
-            {/* <div className={getClassNames(meta === Meta.GoBattleLeague)}
-                onClick={() => setMetaFct(Meta.GoBattleLeague)}
-            >
-                {<Translate id={`metas.${Meta.GoBattleLeague}`} />}
-            </div> */}
-            {/* <div className={getClassNames(meta === Meta.Rose)}
-                onClick={() => setMetaFct(Meta.Rose)}
-            >
-                {<Translate id={`metas.${Meta.Rose}`} />}
-            </div>
-            <div className={getClassNames(meta === Meta.Toxic)}
-                onClick={() => setMetaFct(Meta.Toxic)}
-            >
-                {<Translate id={`metas.${Meta.Toxic}`} />}
-            </div> */}
-        </div>
     );
 }
--- src/i18n/LanguageSwitcher.tsx
+++ src/i18n/LanguageSwitcher.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useI18n } from "./hooks";
 import Select from '@material-ui/core/Select';
-import classNames from "classnames";
 import { MenuItem } from "@material-ui/core";
 
 const langs = {
@@ -12,15 +11,6 @@ const langs = {
 
 const LanguageSwitcher: React.FC = () => {
   const { lang, setLang } = useI18n();
-
-  const getClassNames = (selected: boolean) => {
-    const componentClasses = classNames({
-        "languageButton": true,
-        "selectedButton": selected
-    });
-
-    return componentClasses;
-  }
   
   return (
     <div className="languageButtonContainer">
