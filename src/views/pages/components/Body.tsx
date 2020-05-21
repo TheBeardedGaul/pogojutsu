66883993a972e68235ec8495e35427106c505373
TheBeardedGaul <florent.tetaud@gmail.com>
2020-05-21 19:13:11 +0200

    MVP: breadcrumps

--- src/views/pages/components/Body.tsx
+++ src/views/pages/components/Body.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { RouterComponent } from "../../../routes/RouterComponent";
+import { Breadcrumb } from "./breadcrumb/Breadcrumb";
 
 export const Body: React.FC = () => {
-    return (
-        <body className="AppBody">
-            <RouterComponent />
-        </body>
-    );
-}
+  return (
+    <body className="AppBody">
+      <Breadcrumb />
+      <RouterComponent />
+    </body>
+  );
+};
