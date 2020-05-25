f8c195edc9dc9757405d6f6f1a70862befe8176c
TheBeardedGaul <florent.tetaud@gmail.com>
2020-05-25 10:48:28 +0200

    MVP : Sorcerous Cup is up

--- src/Meta/MetaSwitcher.tsx
+++ src/Meta/MetaSwitcher.tsx
@@ -5,25 +5,40 @@ import { Select, MenuItem } from "@material-ui/core";
 import "./sass/MetaSwitcher.scss";
 
 export interface MetaSwitcherProps {
-    meta: Meta;
-    setMetaFct: (meta: Meta) => void;
+  meta: Meta;
+  setMetaFct: (meta: Meta) => void;
 }
 
-export const MetaSwitcher: React.FC<MetaSwitcherProps> = ({meta, setMetaFct}) => {
-
-    return (
-             <Select
-                className="TextSelect"
-                labelId="selectMeta"
-                id="selectMeta"
-                value={meta}
-                onChange={(event) =>  setMetaFct((event.target.value as  Meta))}
-            >
-                <MenuItem className="TextSelect" value={Meta.GoBattleLeague}>{<Translate id={`metas.${Meta.GoBattleLeague}`} />}</MenuItem>
-                <MenuItem className="TextSelect" value={Meta.Rose}>{<Translate id={`metas.${Meta.Rose}`} />}</MenuItem>
-                <MenuItem className="TextSelect" value={Meta.Toxic}>{<Translate id={`metas.${Meta.Toxic}`} />}</MenuItem>
-                <MenuItem className="TextSelect" value={Meta.Voyager}>{<Translate id={`metas.${Meta.Voyager}`} />}</MenuItem>
-                <MenuItem className="TextSelect" value={Meta.Forest}>{<Translate id={`metas.${Meta.Forest}`} />}</MenuItem>
-            </Select>
-    );
-}
+export const MetaSwitcher: React.FC<MetaSwitcherProps> = ({
+  meta,
+  setMetaFct,
+}) => {
+  return (
+    <Select
+      className="TextSelect"
+      labelId="selectMeta"
+      id="selectMeta"
+      value={meta}
+      onChange={(event) => setMetaFct(event.target.value as Meta)}
+    >
+      <MenuItem className="TextSelect" value={Meta.GoBattleLeague}>
+        {<Translate id={`metas.${Meta.GoBattleLeague}`} />}
+      </MenuItem>
+      <MenuItem className="TextSelect" value={Meta.Rose}>
+        {<Translate id={`metas.${Meta.Rose}`} />}
+      </MenuItem>
+      <MenuItem className="TextSelect" value={Meta.Toxic}>
+        {<Translate id={`metas.${Meta.Toxic}`} />}
+      </MenuItem>
+      <MenuItem className="TextSelect" value={Meta.Voyager}>
+        {<Translate id={`metas.${Meta.Voyager}`} />}
+      </MenuItem>
+      <MenuItem className="TextSelect" value={Meta.Forest}>
+        {<Translate id={`metas.${Meta.Forest}`} />}
+      </MenuItem>
+      <MenuItem className="TextSelect" value={Meta.Sorcerous}>
+        {<Translate id={`metas.${Meta.Sorcerous}`} />}
+      </MenuItem>
+    </Select>
+  );
+};
